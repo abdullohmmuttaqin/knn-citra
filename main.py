a728cfe44d6b7c63f4ce5a9685ed501d6b6ec21f
abdullohmmuttaqin <abdullohmmuttaqin.09@mail.com>
2026-04-20 15:33:43 +0700

    menambahkan akurasi di main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 data = []
 labels = []
