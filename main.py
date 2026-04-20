c3ac600c257197dc27cbb7564015b2444d78ffb3
abdullohmmuttaqin <abdullohmmuttaqin.09@mail.com>
2026-04-20 13:48:10 +0700

    menambahkan syntax di main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import os
+from sklearn.neighbors import KNeighborsClassifier
+
+data = []
+labels = []
+
+# Load dataset
+dataset_path = "dataset"
+
+for label in os.listdir(dataset_path):
+    folder_path = os.path.join(dataset_path, label)
+
+    for img_name in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, img_name)
+        img = cv2.imread(img_path)
+
+        if img is None:
+            continue
+
+        img = cv2.resize(img, (64, 64))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        data.append(img.flatten())
+        labels.append(label)
+
+data = np.array(data)
+
+# Model KNN
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(data, labels)
+
+# Test gambar
+test_img = cv2.imread("test/test.jpg")
+test_img = cv2.resize(test_img, (64, 64))
+test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+prediction = knn.predict([test_img.flatten()])
+
+print("Hasil:", prediction[0])
