4dc485c4200fb206bc47f8e25a4c63f6434871bf
abdullohmmuttaqin <abdullohmmuttaqin.09@mail.com>
2026-04-21 20:47:45 +0700

    memberi baris kosong di bawah import

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
